3d1674381d8154d0c975a60591e527f75b6bc607
ekky2412 <ekky.raharjo@gmail.com>
2021-03-15 18:49:08 +0700

    fix fpdf

--- application/libraries/fpdf.php
+++ application/libraries/fpdf.php
@@ -1,8 +1,8 @@
 <?php
-class Fpdf {
+// class Fpdf {
 
-    function __construct() {
-        include_once APPPATH . '/third_party/fpdf.php';
-    }
-}
+//     function __construct() {
+//         include_once APPPATH . '/third_party/fpdf.php';
+//     }
+// }
 ?>
