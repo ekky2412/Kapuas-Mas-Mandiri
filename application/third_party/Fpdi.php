93bb4052080a4209b3b9cbea4002ccb7b210c55b
ekky2412 <ekky.raharjo@gmail.com>
2021-03-15 19:09:01 +0700

    fix fpdf

--- application/third_party/Fpdi.php
+++ application/third_party/Fpdi.php
@@ -7,7 +7,7 @@
  * @license   http://opensource.org/licenses/mit-license The MIT License
  */
 
-namespace setasign\Fpdi\src;
+namespace setasign\fpdi\src;
 
 use setasign\Fpdi\PdfParser\CrossReference\CrossReferenceException;
 use setasign\Fpdi\PdfParser\PdfParserException;
