4a813120f5b1172142861cff349d48c105b28426
ekky2412 <ekky.raharjo@gmail.com>
2021-03-15 18:59:32 +0700

    fix fpdf

--- application/libraries/Fpdi.php
+++ application/libraries/Fpdi.php
@@ -3,7 +3,7 @@ class Fpdi {
 
     function __construct() {
         include_once APPPATH . '/third_party/fpdf.php';
-        include_once APPPATH . '/third_party/fpdi.php';
+        include_once APPPATH . '/third_party/Fpdi.php';
         include_once APPPATH . '/third_party/autoload.php';
     }
 }
